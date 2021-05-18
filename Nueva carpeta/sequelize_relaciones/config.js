e8357a3953f3f03147683ea734227ca1a51ea5aa
linkobe24 <78578285+linkobe24@users.noreply.github.com>
2021-05-17 20:04:55 -0600

    Delete Nueva carpeta directory

--- Nueva carpeta/sequelize_relaciones/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database:{
-        username: "linkobe24",
-        password: "123456",
-        database: "sequelize",
-        host: "DESKTOP-AQMFC8J",
-        port: 1433
-    }
-}
